eaafa40af1d0c19cd12b25aab31227fa90f0713f
jaydevux <hello@jaydevux.com>
2019-12-30 16:44:10 -0500

    Adding files

---
+++ js/switch.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var switchButton 			= document.querySelector('.switch-button');
+var switchBtnRight 			= document.querySelector('.switch-button-case.right');
+var switchBtnLeft 			= document.querySelector('.switch-button-case.left');
+var activeSwitch 			= document.querySelector('.active');
+
+function switchLeft(){
+	switchBtnRight.classList.remove('active-case');
+	switchBtnLeft.classList.add('active-case');
+	activeSwitch.style.left 						= '0%';
+}
+
+function switchRight(){
+	switchBtnRight.classList.add('active-case');
+	switchBtnLeft.classList.remove('active-case');
+	activeSwitch.style.left 						= '50%';
+}
+
+switchBtnLeft.addEventListener('click', function(){
+	switchLeft();
+}, false);
+
+switchBtnRight.addEventListener('click', function(){
+	switchRight();
+}, false);
+
+
+
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'block')
+       e.style.display = 'none';
+    else
+       e.style.display = 'block';
+ }
+
+ function toggle_invisibility(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'none')
+       e.style.display = 'block';
+    else
+       e.style.display = 'none';
+ }
